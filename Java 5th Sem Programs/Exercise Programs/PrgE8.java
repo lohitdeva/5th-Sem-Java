911cca162f097c726a4b4583b252ccbbe26a5e7f
Lohit Deva <66935143+lohitdeva@users.noreply.github.com>
2021-11-16 23:04:31 +0530

    Update PrgE8.java

--- Java 5th Sem Programs/Exercise Programs/PrgE8.java
+++ Java 5th Sem Programs/Exercise Programs/PrgE8.java
@@ -26,5 +26,23 @@ class PrgE8 {
 		}
 		int odd_nums[] = new int [odd_range];
 		int even_nums[] = new int [even_range];
+		int i = 0, j = 0;
+		while (low <= up) {
+			if (low % 2 == 0) {
+				even_nums[i] = low;
+				i++;
+			}
+			else {
+				odd_nums[j] = low;
+				j++;
+			}
+			low++;
+		}
+		System.out.println("The odd numbers are: ");
+		for (int z = 0; z < odd_range; z++)
+			System.out.print(odd_nums[z] + " ");
+		System.out.println("\nThe even numbers are: ");
+		for (int z = 0; z < even_range; z++)
+			System.out.print(even_nums[z] + " ");
 	}
 }
