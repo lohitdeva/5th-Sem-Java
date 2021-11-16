1a792d6e3dd7dda58370dfa6808baf2e0dbf5728
Lohit Deva <66935143+lohitdeva@users.noreply.github.com>
2021-11-16 12:56:40 +0530

    Update and rename PrgE4.java to PrgE5.java

--- Java 5th Sem Programs/Exercise Programs/PrgE5.java
+++ Java 5th Sem Programs/Exercise Programs/PrgE5.java
@@ -2,7 +2,7 @@
 
 import java.io.*;
 
-class PrgE4 {
+class PrgE5 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a[] = new int [4];
